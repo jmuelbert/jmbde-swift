9b0648a90cbc5d63aeb83dc5894f458247db3b02
Jürgen <juergen.muelbert@gmail.com>
2023-11-06 13:25:22 +0100

    fix: issue with building docs

--- docs/src/env.d.ts
+++ docs/src/env.d.ts
@@ -1,1 +1,10 @@
-import * as astro_client_types from 'astro/client'
+/// <reference types="astro/client" />
+/// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+    readonly GITHUB_TOKEN: string | undefined
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
