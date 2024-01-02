f5c51706ff933b6af3cdb2b8aaf6f1a0c6232d0f
Jürgen Mülbert <juergen.muelbert@outlook.de>
2024-01-02 12:27:44 +0100

    fix: resolve some issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,5 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  overrides: [
-    // ...
-  ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  root: true,
+  extends: [
+    'semistandard'
+  ]
 };
--- docs/src/env.d.ts
+++ docs/src/env.d.ts
@@ -1,5 +1,5 @@
 // <reference path="../.astro/types.d.ts" /> /
 // <reference types="astro/client" /> //
 
-import { path } from '../.astro/types.d.ts'
-import { types } from 'astro/client'
+import { path } from '../.astro/types.d.ts' // eslint-disable-line no-use-before-define
+import { types } from 'astro/client' // eslint-disable-line no-use-before-define
